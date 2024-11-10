7cb4001d589ad5db0774ba01f4c5ddbb6af76ef8
Roc Lai <laipeng668@qq.com>
2024-11-10 12:45:24 +0800

    修改脚本

--- scripts/roc-script.sh
+++ scripts/roc-script.sh
@@ -2,7 +2,7 @@
 sed -i 's/192.168.1.1/192.168.2.1/g' package/base-files/files/bin/config_generate
 
 # 修改亚瑟LED为绿色
-sed -i 's/&status_blue/\&status_green/g' target/linux/qualcommax/files/arch/arm64/boot/dts/qcom/ipq6000-re-ss-01.dts
+# sed -i 's/&status_blue/\&status_green/g' target/linux/qualcommax/files/arch/arm64/boot/dts/qcom/ipq6000-re-ss-01.dts
 
 # 移除要替换的包
 rm -rf feeds/packages/net/alist
