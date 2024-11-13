8af98ef570133faea28365af3f119a15f4039a25
Roc Lai <laipeng668@qq.com>
2024-11-13 19:47:33 +0800

    修改太乙LED颜色

--- scripts/roc-script.sh
+++ scripts/roc-script.sh
@@ -1,8 +1,11 @@
 # 修改默认IP
 sed -i 's/192.168.1.1/192.168.2.1/g' package/base-files/files/bin/config_generate
 
-# 修改亚瑟LED为绿色
+# 修改亚瑟运行LED为绿色
 # sed -i 's/&status_blue/\&status_green/g' target/linux/qualcommax/files/arch/arm64/boot/dts/qcom/ipq6000-re-ss-01.dts
+
+# 修改太乙启动LED为绿色，运行LED为蓝色(VIKINGYFY项目)
+# sed -i 's/boot/roc/g; s/running/boot/g; s/roc/running/g' target/linux/qualcommax/files/arch/arm64/boot/dts/qcom/ipq6010-re-cs-07.dts
 
 # 移除要替换的包
 rm -rf feeds/packages/net/alist
