45f3d3c85ded73b05be114741d8347b12a870b6e
x2009again <x2009again@126.com>
2024-11-14 22:02:26 +0800

    修改说明，去掉vlmcsd

--- scripts/base-script.sh
+++ scripts/base-script.sh
@@ -1,5 +1,5 @@
 # 修改默认IP
-# sed -i 's/192.168.1.1/192.168.2.1/g' package/base-files/files/bin/config_generate
+sed -i 's/192.168.1.1/192.168.5.1/g' package/base-files/files/bin/config_generate
 
 # 修改亚瑟运行LED为绿色
 # sed -i 's/&status_blue/\&status_green/g' target/linux/qualcommax/files/arch/arm64/boot/dts/qcom/ipq6000-re-ss-01.dts
